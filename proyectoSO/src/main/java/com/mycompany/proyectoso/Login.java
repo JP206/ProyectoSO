ad530377faf6323636d973e34e80090b58454c55
JP206 <88661539+JP206@users.noreply.github.com>
2022-06-17 17:51:28 -0300

    cambios login

--- proyectoSO/src/main/java/com/mycompany/proyectoso/Login.java
+++ proyectoSO/src/main/java/com/mycompany/proyectoso/Login.java
@@ -13,16 +13,18 @@ public class Login {
     
     public boolean esUsuario(String nombre, String contraseña){
         ManejadorArchivosGenerico manejador = new ManejadorArchivosGenerico();
-        String[] usuarios = manejador.leerArchivo("src\\usuarios.txt");     // Guardo usuarios y contraseñas en array de strings. Usuarios están en txt llamado usuarios en la carpeta src, un usuario o contraseña por linea.
+        String[] usuarios = manejador.leerArchivo("src\\usuarios.txt");     // Guardo usuarios y contraseñas en array de strings. Usuarios están en txt llamado usuarios en la carpeta src, un usuario:contraseña por linea
+        String[] usuariosSplit;
         int i = 0;
         boolean nombreValido = false;
         boolean contraseñaValida = false;
         boolean resultado = nombreValido && contraseñaValida;   // El resultado es la conjuncion de ambas condiciones
         while (i < usuarios.length && !resultado){      // Si el resultado es verdadero, ya corta el while
-            if (usuarios[i].equals(nombre)){
+            usuariosSplit = usuarios[i].split(":");
+            if (usuariosSplit[0].equals(nombre)){
                 nombreValido = true;        
             }
-            if (usuarios[i].equals(contraseña)){
+            if (usuariosSplit[1].equals(contraseña)){
                 contraseñaValida = true;
             }
             resultado = nombreValido && contraseñaValida;
@@ -33,7 +35,8 @@ public class Login {
     
     public void agregarUsuario(String nombre, String contraseña){
         ManejadorArchivosGenerico manejador = new ManejadorArchivosGenerico();
-        String[] usuario = {nombre, contraseña};    // Creo arreglo de strings con usuario y contraseña para escribir en el txt
-        manejador.escribirArchivo("src\\usuarios.txt", usuario);    // Escribo el txt "usuarios" en la carpeta src del proyecto con el usuario nuevo
+        String usuario = nombre + ":" + contraseña;
+        String[] usuarios = {usuario};    // Creo arreglo de strings con usuario y contraseña para escribir en el txt
+        manejador.escribirArchivo("src\\usuarios.txt", usuarios);    // Escribo el txt "usuarios" en la carpeta src del proyecto con el usuario nuevo
     }   
 }
--- proyectoSO/src/main/java/com/mycompany/proyectoso/ProyectoSO.java
+++ proyectoSO/src/main/java/com/mycompany/proyectoso/ProyectoSO.java
@@ -56,6 +56,5 @@ public class ProyectoSO {
             System.out.println(aux.processName);
         }
         System.out.println("---------------------------------");
-        
     }
 }
